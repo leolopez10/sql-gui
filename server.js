e76a076641c2d00e320c3e6c6c41dee01873897e
lil L <57925562+leolopez10@users.noreply.github.com>
2020-07-01 15:16:54 -0500

    played with basic query calls to sqlite database

--- server.js
+++ server.js
@@ -15,6 +15,55 @@ let db = new sqlite.Database(
   }
 );
 
+// Queries
+// Get all
+db.all(
+  `SELECT DISTINCT Name name FROM playlists ORDER BY name`,
+  [],
+  (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach(row => {
+      console.log(row.name);
+    });
+  }
+);
+
+// Get method
+let playListId = 5;
+db.get(
+  `SELECT PlaylistId id, Name name FROM playlists WHERE PlaylistId = ?`,
+  [playListId], // the number related to the playlist
+  (err, row) => {
+    if (err) {
+      return console.log(err.message);
+    }
+    if (row) {
+      console.log(row.id, row.name);
+    } else {
+      console.log(`No playlist found with the id ${playListId}`);
+    }
+  }
+);
+
+// Each method
+db.each(
+  `SELECT FirstName firstName,
+LastName lastName,
+Email email
+FROM customers
+WHERE Country = ?
+ORDER BY FirstName`,
+  ['USA'],
+  (err, row) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`${row.firstName} ${row.lastName} - ${row.email}`);
+  }
+);
+
 // Close connection
 db.close(err => {
   if (err) {
