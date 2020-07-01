986fb284ca61b8ca11be8a9b9b3f142c3e42201c
lil L <57925562+leolopez10@users.noreply.github.com>
2020-07-01 16:44:37 -0500

    got the fron end communicating to the backend

--- server.js
+++ server.js
@@ -3,22 +3,36 @@ require('dotenv').config();
 const express = require('express');
 const sqlite = require('sqlite3').verbose();
 
-// Establish connection
-let db = new sqlite.Database(
-  './models/chinook.db',
-  sqlite.OPEN_READWRITE,
-  err => {
-    if (err) {
-      return console.log(err.message);
-    }
-    console.log('Connected to SQlite Database');
-  }
-);
+const app = express();
+let PORT = process.env.PORT || 5000;
 
-// Close connection
-db.close(err => {
-  if (err) {
-    return console.log(err.message);
-  }
-  console.log('Closed the database connection');
-});
+// Init Middleware (body parser)
+app.use(express.json({ extended: false }));
+
+// Lets check if our backend is running (remove this for deployment)
+app.get('/', (req, res) => res.send('API RUNNING!!!'));
+
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
+
+// // ========================================
+// // Database connections
+// // ========================================
+// // Establish SQLite connection
+// let db = new sqlite.Database(
+//   './models/chinook.db',
+//   sqlite.OPEN_READWRITE,
+//   err => {
+//     if (err) {
+//       return console.log(err.message);
+//     }
+//     console.log('Connected to SQlite Database');
+//   }
+// );
+
+// // Close SQLite connection
+// db.close(err => {
+//   if (err) {
+//     return console.log(err.message);
+//   }
+//   console.log('Closed the database connection');
+// });
