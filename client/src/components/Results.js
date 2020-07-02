24acda37776a7dcff1709bb3bddf3f065966dbd9
lil L <57925562+leolopez10@users.noreply.github.com>
2020-07-02 18:31:39 -0500

    fixed an edging issue

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -6,34 +6,32 @@ import { Container, Col, Row, ListGroup, ListGroupItem } from 'reactstrap';
 function Results() {
   return (
     <div className='mb-3'>
-      <Container>
-        <h5>Results</h5>
-        <Col>
-          <Row>
-            <ListGroup
-              style={{
-                width: '100%'
-              }}
-            >
-              <ListGroupItem color='info'>
-                Which albums have the most tracks
-              </ListGroupItem>
-              <ListGroupItem color='info'>
-                Which albums have the most track time?
-              </ListGroupItem>
-              <ListGroupItem color='info'>
-                Which artists have the most track time?
-              </ListGroupItem>
-              <ListGroupItem color='info'>
-                Which tracks appears on the most playlists?
-              </ListGroupItem>
-              <ListGroupItem color='info'>
-                Which playlists have the most track time?
-              </ListGroupItem>
-            </ListGroup>
-          </Row>
-        </Col>
-      </Container>
+      <h5>Results</h5>
+      <Col>
+        <Row>
+          <ListGroup
+            style={{
+              width: '100%'
+            }}
+          >
+            <ListGroupItem color='info'>
+              Which albums have the most tracks
+            </ListGroupItem>
+            <ListGroupItem color='info'>
+              Which albums have the most track time?
+            </ListGroupItem>
+            <ListGroupItem color='info'>
+              Which artists have the most track time?
+            </ListGroupItem>
+            <ListGroupItem color='info'>
+              Which tracks appears on the most playlists?
+            </ListGroupItem>
+            <ListGroupItem color='info'>
+              Which playlists have the most track time?
+            </ListGroupItem>
+          </ListGroup>
+        </Row>
+      </Col>
     </div>
   );
 }
